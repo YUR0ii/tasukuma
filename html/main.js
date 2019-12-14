751e1178177e57e28d66317c5be86833e217651b
yeet <janepetrov@protonmail.ch>
2019-12-14 15:30:10 -0500

    last commit!

--- html/main.js
+++ html/main.js
@@ -12,8 +12,48 @@ function populate_slots() {
         if (!window.slots[slot].id) window.slots[slot].id = current_max_slot_id++;
         //console.log("populate! slot should be " + window.slots[slot].id, window.slots[slot]);
         child.id = ("itemslot-" + window.slots[slot].id);
-        child.innerHTML = JSON.stringify(window.slots[slot]);
+        child.innerHTML = "<img src=" + window.slots[slot].image +  " style=\"margin: 30px; width:auto; height:inherit;\"></img>" +
+        "<p style=\"margin-top: -40px;\">" + window.slots[slot].fact + "</p>";
+        child.setAttribute("style", "height: 150px; width: auto;");
         slots_container.appendChild(child);
+    }
+}
+
+var bears = [
+    "./resources/eating_bear.png",
+    "./resources/normal_bear.png",
+    "./resources/sleeping_bear.png"
+]
+
+var facts = [
+    "2050 is too long to wait to go carbon neutral. We need to act NOW or it will be too late.",
+    "100 companies create 71% of greenhouse gas emissions, so call your representatives about corporate responsibility.",
+    "Climate refugees already number in the tens of millions, expected to reach over a billion by 2050 if things don't improve.",
+    "CO2 levels are about 20% higher than they have been in the past 500 thousand years, and more than double what they were 100 years ago.",
+    "The earth's average temperature has increased before, but never this much and never this quickly since the atmosphere settled."
+]
+
+function attract_bear() {
+    var img = bears[Math.floor(Math.random() * bears.length)];
+    var fact = facts[Math.floor(Math.random() * facts.length)];
+
+    window.slots.push({
+        id: current_max_slot_id++,
+        image: img,
+        fact: fact,
+        type: "unimpl",
+        bear_present: true,
+        bear_type: img,
+        bear_state: null
+    });
+    save_slots();
+    populate_slots();
+}
+
+function check_score() {
+    console.log(scores.supporters, (100 * current_max_slot_id));
+    if(window.scores.supporters > (100 * current_max_slot_id)) {
+        attract_bear();
     }
 }
 
@@ -47,6 +87,8 @@ function load_data() {
 function add_test_slot() {
     window.slots.push({
         id: current_max_slot_id++,
+        image: "./resources/normal_bear.png",
+        fact: "TEST_FACT",
         type: "TEST_ITEM",
         bear_present: false,
         bear_type: "TEST_BEAR",
@@ -91,6 +133,7 @@ function game_loop() {
     window.scores.supporters += window.scores.supporter_increment;
     save_data();
     load_values();
+    check_score();
 }
 
 /*
